7a6ac06f5056480deb0cabd06432ccb495196b0b
kaioh33 <gupajibril@gmail.com>
2022-07-14 15:47:32 +0100

    add functions solution

---
+++ src/Solutions/chapter 4/ch4.4/functions_2.rs
@@ -0,0 +1,18 @@
+//fn main() {
+    //print();
+//}
+ 
+// Replace i32 with another type
+//fn print() -> i32 {
+    //println!("Success!");
+//}
+ 
+fn main() {
+    print();
+}
+ 
+ // Replace i32 with another type
+fn print() -> () {
+    println!("Success!");
+}
+ 
---
+++ src/Solutions/chapter 4/ch4.4/functions_3.rs
@@ -0,0 +1,26 @@
+// Solve it in two ways
+// DON'T let `println!` works
+//fn main() {
+    //never_return();
+
+    //println!("Failed!");
+//}
+
+//fn never_return() -> ! {
+    // Implement this function, don't modify the fn signatures
+    
+//}
+
+// Solve it in two ways
+// DON'T let `println!` works
+fn main() {
+    never_return();
+
+    println!("Failed!");
+}
+
+fn never_return() -> ! {
+    // Implement this function, don't modify the fn signatures
+    panic!("never return!");
+}
+
