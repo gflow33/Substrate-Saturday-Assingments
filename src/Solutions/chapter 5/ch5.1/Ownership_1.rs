5760404740ab27c0b5d2704c56b0bdfa14124e3c
kaioh33 <gupajibril@gmail.com>
2022-07-15 06:15:45 +0100

    add solutions

---
+++ src/Solutions/chapter 5/ch5.1/Ownership_1.rs
@@ -0,0 +1,14 @@
+//fn main() {
+    // Use as many approaches as you can to make it work
+    //let x = String::from("hello, world");
+    //let y = x;
+    //println!("{},{}",x,y);
+//}
+
+
+fn main() {
+    // Use as many approaches as you can to make it work
+    let x = String::from("hello, world");
+    let y = x.clone();
+    println!("{},{}",x,y);
+}
