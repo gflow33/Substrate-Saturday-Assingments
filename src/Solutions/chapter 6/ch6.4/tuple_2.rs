47839bc7dbd74cc1b7a22511cd97638761642b39
kaioh33 <gupajibril@gmail.com>
2022-07-15 17:20:11 +0100

    add tuple solution

---
+++ src/Solutions/chapter 6/ch6.4/tuple_2.rs
@@ -0,0 +1,16 @@
+// Make it work
+//fn main() {
+    //let t = ("i", "am", "sunface");
+    //assert_eq!(t.1, "sunface");
+
+    //println!("Success!");
+//}
+
+
+// Make it work
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.2, "sunface");
+
+    println!("Success!");
+}
