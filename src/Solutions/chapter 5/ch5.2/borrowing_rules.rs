6c89f416752236a61efdfdc9af5d37967694833d
kaioh33 <gupajibril@gmail.com>
2022-07-15 06:36:18 +0100

    add solution

---
+++ src/Solutions/chapter 5/ch5.2/borrowing_rules.rs
@@ -0,0 +1,26 @@
+
+// Remove something to make it work
+// Don't remove a whole line !
+//fn main() {
+    //let mut s = String::from("hello");
+
+    //let r1 = &s;
+    //let r2 = &s;
+
+    //println!("{}, {}", r1, r2);
+
+    //println!("Success!");
+//}
+
+// Remove something to make it work
+// Don't remove a whole line !
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+
+    println!("{}, {}", r1, r2);
+
+    println!("Success!");
+}
