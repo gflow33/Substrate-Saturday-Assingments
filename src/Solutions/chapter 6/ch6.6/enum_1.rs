a9b964c5243ff9abe2768484da1b0fa5caee95ce
kaioh33 <gupajibril@gmail.com>
2022-07-16 12:55:58 +0100

    add enum solution

---
+++ src/Solutions/chapter 6/ch6.6/enum_1.rs
@@ -0,0 +1,60 @@
+/* 
+// Fix the errors
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+enum Number1 {
+    Zero = 0,
+    One,
+    Two,
+}
+
+// C-like enum
+enum Number2 {
+    Zero = 0.0,
+    One = 1.0,
+    Two = 2.0,
+}
+
+
+fn main() {
+    // An enum variant can be converted to a integer by `as`
+    assert_eq!(Number::One, Number1::One);
+    assert_eq!(Number1::One, Number2::One);
+
+    println!("Success!");
+} 
+*/
+
+
+// Fix the errors
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+enum Number1 {
+    Zero = 0,
+    One,
+    Two,
+}
+
+// C-like enum
+enum Number2 {
+    Zero = 0,
+    One = 1,
+    Two = 2,
+}
+
+
+fn main() {
+    // An enum variant can be converted to a integer by `as`
+    assert_eq!(Number::One as i32, Number1::One as i32);
+    assert_eq!(Number1::One as i32, Number2::One as i32);
+
+    println!("Success!");
+} 
