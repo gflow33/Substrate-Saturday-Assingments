9f7f488426f1e1eb07b8167df3621e3438223649
kaioh33 <gupajibril@gmail.com>
2022-07-22 12:11:56 +0100

    add commits

---
+++ src/Solutions/chapter 10/ch10.2/const_generics_2.rs
@@ -0,0 +1,26 @@
+/* 
+// Fill in the blanks to make it work.
+fn print_array<__>(__) {
+    println!("{:?}", arr);
+}
+fn main() {
+    let arr = [1, 2, 3];
+    print_array(arr);
+
+    let arr = ["hello", "world"];
+    print_array(arr);
+}
+*/
+// Fill in the blanks to make it work.
+fn print_array<T, const N: usize>(arr: [T; N])
+where T: std::fmt::Debug
+{
+    println!("{:?}", arr);
+}
+fn main() {
+    let arr = [1, 2, 3];
+    print_array(arr);
+
+    let arr = ["hello", "world"];
+    print_array(arr);
+}
