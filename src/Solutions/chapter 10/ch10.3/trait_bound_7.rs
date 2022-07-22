5747e011dcc9ca7c8ddad373d0e89f12f616de94
kaioh33 <gupajibril@gmail.com>
2022-07-22 22:29:40 +0100

    add trait objects solution

---
+++ src/Solutions/chapter 10/ch10.3/trait_bound_7.rs
@@ -0,0 +1,25 @@
+/* fn main() {
+    assert_eq!(sum(1, 2), 3);
+}
+
+// Implement `fn sum` with trait bound in two ways.
+fn sum<T>(x: T, y: T) -> T {
+    x + y
+}
+*/
+fn main() {
+    assert_eq!(sum(1, 2), 3);
+}
+
+// Implement `fn sum` with trait bound in two ways.
+fn sum<T>(x: T, y: T) -> T 
+where T: std::ops::Add<Output = T>,
+{
+    x + y
+}
+
+// Implement `fn sum` with trait bound in two ways.
+fn sum_1<T: std::ops::Add<Output = T>>(x: T, y: T) -> T {
+    x + y
+}
+
